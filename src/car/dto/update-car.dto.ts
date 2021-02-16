5fc5d46eaf2624e99420ff10ce5f089f2afb80f2
sadek666 <you@example.com>
2021-02-16 13:43:50 +0100

    update-car.dto fix, update-mechanic.dto fix.

--- src/car/dto/update-car.dto.ts
+++ src/car/dto/update-car.dto.ts
@@ -1,4 +1,27 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCarDto } from './create-car.dto';
+import { IsNotEmpty, IsNumber, IsString, Length } from 'class-validator';
 
-export class UpdateCarDto extends PartialType(CreateCarDto) {}
+export class UpdateCarDto {
+  @IsNotEmpty()
+  @Length(2, 100)
+  @IsString()
+  brand: string;
+
+  @IsNotEmpty()
+  @Length(2, 100)
+  @IsString()
+  model: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  productionYear: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 500)
+  repairNote: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 500)
+  color: string;
+}
--- src/mechanic/dto/update-mechanic.dto.ts
+++ src/mechanic/dto/update-mechanic.dto.ts
@@ -4,5 +4,4 @@ import { CreateMechanicDto } from './create-mechanic.dto';
 export class UpdateMechanicDto extends PartialType(CreateMechanicDto) {
   mechanicName: string;
   mechanicLastName: string;
-  userLastName: string;
 }
